bac0d8b8df5c8f65aa0e911242582c3a3911e1b0
butcherZ <daydreamnation@live.com>
2018-02-04 15:29:21 +0100

    realloc and parsing

--- zyuan/src/read.c
+++ zyuan/src/read.c
@@ -23,45 +23,70 @@ typedef	struct s_vector
 		int		z;
 }				t_vector;
 
-//char	**realloc_and_append(char **)
+
+/*
+1) New variable to return t_vector new_vector *
+2) We malloc this variable + 1 from the vector
+3) We copy vector to new_vector
+4) We append the tab
+5) We free vector
+6) we return
+*/
+t_vector	*realloc_and_append_struct(t_vector *vectors, char **tab, int elements_number, int width)
+{
+	int i;
+	t_vector *new_vector;
+
+	i = 0;
+	new_vector = malloc(sizeof(t_vector) * (elements_number + 1));
+	while (i < elements_number)
+	{
+		new_vector[i] = vectors[i];
+		i++;
+	}
+	new_vector[i].x = width;
+	new_vector[i].z = ft_atoi(tab[width]);
+	free(vectors);
+	return new_vector;
+}
+
 char		**my_get_line(int fd, char **line)
 {
 		char	**tab;
 		t_vector	*vector;
 		int		height;
 		int		width;
+		int 	i;
 
+		i = 0;
 		height = 0;
-		vector = malloc(sizeof(t_vector) * 20);
+		vector = malloc(sizeof(t_vector) * 1);
 		while (get_next_line(fd, line) == 1)
 		{
-			printf("size of line is %zu\n", ft_strlen(*line));
 			vector[height].y = height;
-		  // printf("y is %d\n", vector[height].y);	
 			tab = ft_strsplit(*line, ' ');
+
 			if (tab)
 			{
 				width = 0;
 				while (tab[width])
 				{
-					vector[height].x = width;
-					vector[height].z = ft_atoi(tab[width]);
-			//		printf("x is %d\n", vector[width].x);
-			//		printf("z is %d\n", vector[width].z);
+					// vector[i].x = width;
+					// vector[i].z = ft_atoi(tab[width]);
+					vector = realloc_and_append_struct(vector, tab, i, width);
+
 					width++;
+					i++;
 				}
-			printf("width is %d\n", width);
 			}
 			height++;
 		}
-		printf("height is %d\n", height);
-		for (int i = 0; i < height; i++)
+
+		for (int i = 0; i < 20 * 20; i++)
 		{
-			for (int i = 0; i < width; i++)
-			{
-				printf("%d", vector[i].z);
-			}
-			printf("\n");
+				printf("%d ", vector[i].z);
+				if (i % 20 == 0 && i != 0)
+					printf("\n");
 		}
 	return (tab);
 }
